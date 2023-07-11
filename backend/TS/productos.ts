4c68110fb18f7e3816afb31895e71a32d85df1f5
ADEP-123 <andep210399@gmail.com>
2023-07-10 19:14:27 -0500

    feat: :sparkles: TS productos

---
+++ backend/TS/productos.ts
@@ -0,0 +1,53 @@
+import { Expose, Type, Transform } from "class-transformer";
+
+export class productos {
+    @Expose({ name: "ID" })
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+    ID: number;
+
+    @Expose({ name: "NOMBRE" })
+    @Type(() => String)
+    NOMBRE: string;
+
+    @Expose({ name: "DESCRIPCION" })
+    @Type(() => String)
+    DESCRIPCION: string;
+
+    @Expose({ name: "ESTADO" })
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+    ESTADO: number;
+
+    @Expose({ name: "CREADOR" })
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+    CREADOR: number;
+
+    @Expose({ name: "ACTUALIZADOR" })
+    @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+    ACTUALIZADOR: number;
+
+    @Expose({ name: "FECHA_CREACION" })
+    @Transform(({ value }) => new Date(value), { toClassOnly: true })
+    FECHA_CREACION: Date;
+
+    @Expose({ name: "FECHA_ACTUALIZACION" })
+    @Transform(({ value }) => new Date(value), { toClassOnly: true })
+    FECHA_ACTUALIZACION: Date;
+
+    @Expose({ name: "FECHA_ELIMINACION" })
+    @Transform(({ value }) => new Date(value), { toClassOnly: true })
+    FECHA_ELIMINACION: Date;
+
+
+
+    constructor(id: number, nombre: string, descripcion: string, estado: number, created_by: number, update_by: number, created_at: Date, updated_at: Date, deleted_at: Date) {
+        this.ID = id;
+        this.NOMBRE = nombre;
+        this.DESCRIPCION = descripcion;
+        this.ESTADO = estado;
+        this.CREADOR = created_by;
+        this.ACTUALIZADOR = update_by;
+        this.FECHA_CREACION = created_at;
+        this.FECHA_ACTUALIZACION = updated_at;
+        this.FECHA_ELIMINACION = deleted_at;
+    }
+}
